a36624014fe2d6c80e227e5c4f5e33906314545a
Abah Gideon <abahgideon111@gmail.com>
2025-11-14 14:37:49 +0100

    updated checkout modal logic

--- components/CheckOutModal.vue
+++ components/CheckOutModal.vue
@@ -255,7 +255,7 @@ const finalBalance = computed(() => {
 
 const isFormValid = computed(() => {
   return checkOutData.value.room_condition && 
-         (finalBalance.value <= 0 || (checkOutData.value.payment_method && checkOutData.value.final_payment > 0))
+         (finalBalance.value <= 0.01 || (finalBalance.value > 0.01 && checkOutData.value.payment_method && checkOutData.value.final_payment > 0))
 })
 
 const formatDate = (dateStr: string) => {
